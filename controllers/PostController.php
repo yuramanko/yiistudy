20158c6d8df43fa5a79db4a00e8e3fee50d9eef1
Yura <manko.yuriy@gmail.com>
2018-05-23 14:32:10 +0300

    iss10 Метаданные страницы

--- controllers/PostController.php
+++ controllers/PostController.php
@@ -27,11 +27,15 @@ class PostController extends AppController{
             debug(Yii::$app->request->post());
             return 'test';
         }
+        $this->view->title = 'Все статьи';
         return $this->render('test');
     }
 
     public function actionShow(){
 //        $this->layout = 'basic';
+        $this->view->title = 'Одна статья!';
+        $this->view->registerMetaTag(['name' => 'keywords', 'content' => 'ключевики...']);
+        $this->view->registerMetaTag(['name' => 'description', 'content' => 'описание страницы...']);
         return $this->render('show');
     }
 
--- views/post/show.php
+++ views/post/show.php
@@ -1,3 +1,11 @@
+<?php
+//$this->title = 'Одна статья';
+?>
+
+<?php $this->beginBlock('block1'); ?>
+    <h1>Заголовок страницы</h1>
+<?php $this->endBlock(); ?>
+
 <h1>Show Action</h1>
 
 <button class="btn btn-success" id="btn">Click me...</button>
